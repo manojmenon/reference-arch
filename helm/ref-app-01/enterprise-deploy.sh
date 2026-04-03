915bfeb638193832f2806ed5eb4ab7d68a170e9d
Manoj Menon <manojmenon@prg01-k8s-181.a3jm.com>
2026-04-03 09:46:15 +0200

    Corrected enterprise-deploy.sh with the correct GIT_HUB_TOKEN

--- helm/ref-app-01/enterprise-deploy.sh
+++ helm/ref-app-01/enterprise-deploy.sh
@@ -1,4 +1,4 @@
-echo "update the GITHUB_TOEKN and then only proceed"
+echo "update the GITHUB_TOKEN and then only proceed"
 read x
 
  kubectl delete ns ent-3t-app-01
@@ -6,7 +6,7 @@ kubectl create ns ent-3t-app-01
 kubectl -n ent-3t-app-01 create secret docker-registry ghcr-secret \
   --docker-server=ghcr.io \
   --docker-username=manojmenon \
-  --docker-password=${GITHUB} \
+  --docker-password=${GITHUB_TOKEN} \
   --docker-email=manojmenon.menon@email.com
 
 kubectl -n ent-3t-app-01 apply -f deploy/k8s/postgres/
