d1e388d77e9eb3d95b3c066925690db14ce9dfd6
Manoj Menon <manojmenon@prg01-k8s-191.a3jm.com>
2026-04-04 10:56:18 +0200

    Check and wait for roll-out

--- helm/ref-app-01/enterprise-deploy.sh
+++ helm/ref-app-01/enterprise-deploy.sh
@@ -1,7 +1,11 @@
 echo "update the GITHUB_TOKEN and then only proceed"
+echo GITHUB_TOKEN=$GITHUB_TOKEN
+echo ""
+echo "Press CR if the above is correct or Ctrl-C to quit"
 read x
 
- kubectl delete ns ent-3t-app-01
+kubectl delete ns ent-3t-app-01 --ignore-not-found
+
 kubectl create ns ent-3t-app-01
 kubectl -n ent-3t-app-01 create secret docker-registry ghcr-secret \
   --docker-server=ghcr.io \
@@ -12,3 +16,10 @@ kubectl -n ent-3t-app-01 create secret docker-registry ghcr-secret \
 kubectl -n ent-3t-app-01 apply -f deploy/k8s/postgres/
 kubectl -n ent-3t-app-01 apply -f deploy/k8s/backend/
 kubectl -n ent-3t-app-01 apply -f deploy/k8s/frontend/
+
+
+kubectl rollout status -n ent-3t-app-01 deployment/enterprise-frontend --timeout=300s
+kubectl rollout status -n ent-3t-app-01 deployment/enterprise-backend --timeout=300s
+
+sudo ufw allow 8080/tcp
+kubectl port-forward -n ent-3t-app-01 svc/enterprise-frontend 8080:80 --address 0.0.0.0
